2a708d2032ef4c157f4f2b67141a2372d7484ffd
edvxndxvi <edvandavi1@hotmail.com>
2024-09-23 15:49:29 -0300

    Adicionando script para ativação de itens na página orçamento

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Função Links Ativos Página Atual
+//Ativar Links do Menu 
 const links = document.querySelectorAll(".header ul a")
 
 function ativarLink(link){
@@ -11,3 +11,16 @@ function ativarLink(link){
 }
 
 links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    if(elemento){
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto)
+
